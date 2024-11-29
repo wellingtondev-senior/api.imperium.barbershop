ba3141ef76d4090334713bad4dfa0a92a1849023
wellingtondev-senior <zendevup@gmail.com>
2024-11-29 17:53:28 -0300

    update

--- src/providers/session-hash/session-hash.controller.ts
+++ src/providers/session-hash/session-hash.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Param, UseGuards, HttpCode } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, HttpCode, Version } from '@nestjs/common';
 import { SessionHashService } from './session-hash.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/decorator/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+import { RoleGuard } from 'src/guards/role.guard';
 
 @ApiTags('session-hash')
 @Controller('session-hash')
 export class SessionHashController {
   constructor(private readonly sessionHashService: SessionHashService) {}
-
+  
+  @Version('1')
   @Get('validate/:hash/:userId')
+  @Roles(Role.MASTER, Role.ADM)
+  @UseGuards(RoleGuard)
   @ApiOperation({ 
     summary: 'Validar hash de confirmação',
     description: 'Valida uma hash de confirmação de email. Se a hash for válida, o email será confirmado.'
